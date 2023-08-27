71ade41747b74a11103d0ee42d20adea10006f59
omerozcetin-infoline <omer.ozcetin@infoline-tr.com>
2023-08-27 23:34:22 +0300

    Add project files.

---
+++ src/Acme.BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
@@ -0,0 +1,25 @@
+﻿using Acme.BookStore.Authors;
+using Acme.BookStore.Books;
+using AutoMapper;
+using static Acme.BookStore.Authors.AuthorAppService;
+
+namespace Acme.BookStore;
+
+public class BookStoreApplicationAutoMapperProfile : Profile
+{
+    public BookStoreApplicationAutoMapperProfile()
+    {
+        /* You can configure your AutoMapper mapping configuration here.
+         * Alternatively, you can split your mapping configurations
+         * into multiple profile classes for a better organization. */
+        CreateMap<Book, BookDto>();
+        CreateMap<CreateUpdateBookDto, Book>();
+        CreateMap<BookDto, CreateUpdateBookDto>();
+        CreateMap<Author, AuthorDto>();
+
+        // ADD a NEW MAPPING
+        CreateMap<CreateAuthorViewModel,
+                    CreateAuthorDto>();
+    }
+
+}
---
+++ src/Acme.BookStore.Domain.Shared/Localization/BookStoreResource.cs
@@ -0,0 +1,9 @@
+﻿using Volo.Abp.Localization;
+
+namespace Acme.BookStore.Localization;
+
+[LocalizationResourceName("BookStore")]
+public class BookStoreResource
+{
+    public const string AuthorAlreadyExists = "BookStore:00001";
+}
